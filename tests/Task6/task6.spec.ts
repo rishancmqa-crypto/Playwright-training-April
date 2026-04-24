014f0dcb33379186c2ac02249d7fb4d5555f05bc
Rishan Cm <rishan.cmqa@gmail.com>
2026-04-24 11:49:59 +0530

    Task 6 completed

---
+++ tests/Task6/task6.spec.ts
@@ -0,0 +1,6 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="login-button1"]').click();
+});
